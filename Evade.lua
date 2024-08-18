e31a6912a1d5e97d8668383fd2904e57a5dcbd86
Cold-Script <166982379+Cold-Script@users.noreply.github.com>
2024-08-18 17:57:05 +0700

    Update Evade.lua

--- Evade.lua
+++ Evade.lua
@@ -1,37 +1,32 @@
-local notificationSound = Instance.new'Sound'
-notificationSound.SoundId = 'rbxassetid://6897623656'
-notificationSound.Volume = 1
-notificationSound.Parent = game.Players.LocalPlayer.PlayerGui
-lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/mstudio45/LinoriaLib/main/Library.lua"))()
-function notify(Text, Duration)
-    lib:Notify(Text, Duration) then
-  notificationSound:Play()
-end
-_spawn = task.spawn
-_spawn(function()
-while wait(.5) do
-for _,v in pairs(workspace.Game.Effects:GetDescendants()) do
-if v.Name == "Tickets" then
+local notify = Instance.new'Sound'
+notify.SoundId = 'rbxassetid://6897623656'
+notify.Volume = 2.5
+notify.Parent = game.Players.LocalPlayer.PlayerGui
+Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/mstudio45/LinoriaLib/main/Library.lua"))()
+for _,v in pairs(workspace.Game.Map:GetDescendants()) do
+if v.Name == "InvisParts" then
 v:Destroy()
-elseif v.Name == "Ingore" then
+elseif v.Name == "ItemSpawns" then
 v:Destroy()
-elseif v.Name == "Powerups" then
+elseif v.Name == "Ignore" then
 v:Destroy()
-elseif v.Name == "Deployables" then
+elseif v.Name == "Navmesh" then
 v:Destroy()
 end
 end
-for _,v in pairs(game.ReplicatedStorage:GetDescendants()) do
-if v.Name == "OtherStorage" then
+for _,v in pairs(workspace.Game.Map.Parts:GetDescendants()) do
+if v.Name == "Lights" then
+v:Destroy()
+elseif v.Name == "MeshPart" then
+v:Destroy()
+elseif v.Name == "Water" then
 v:Destroy()
 end
 end
+for _,v in pairs(workspace.Game.Map:GetDescendants()) do
+if v.Name == "SavedTerrain" then
+v:Destroy()
 end
-end)
-notify("[ YOU HUB ] : Remove Tickets Enabled")
-notify("[ YOU HUB ] : Remove Ingore Enabled")
-notify("[ YOU HUB ] : Remove Powerups Enabled")
-notify("[ YOU HUB ] : Remove Deployables Enabled")
-notify("[ YOU HUB ] : Remove OtherStorage Enabled")
-notify("[ YOU HUB (Warning) ] : Cant Revive Players")
-notify("[ YOU HUB (Warning) ] : By rechedmcvn & GV , Goodluck GamePlay")
+end
+Library:Notify("[ YOU HUB (Warning) ] : By rechedmcvn & GV , Goodluck GamePlay")
+notify:Play()
